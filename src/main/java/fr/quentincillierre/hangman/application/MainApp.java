86aed47399fb7f960b1ed95a7efbf89c0f860282
Benjamin Pitogo <pbjm@passerellesnumeriques.org>
2026-07-27 00:54:14 +0800

    favicon added

--- src/main/java/fr/quentincillierre/hangman/application/MainApp.java
+++ src/main/java/fr/quentincillierre/hangman/application/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -19,6 +20,9 @@ public class MainApp extends Application {
 
         Scene scene = new Scene(root, 900, 600);
         scene.getStylesheets().add(getClass().getResource("menu.css").toExternalForm());
+
+        Image appIcon = new Image(getClass().getResourceAsStream("/videos/HANGCAT FAVICON.png"));
+        primaryStage.getIcons().add(appIcon);
 
         primaryStage.setTitle("HangCat");
         primaryStage.setScene(scene);
